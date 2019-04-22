1350e392dd8dbffcf0d9abeb6681e6f940e99ec1
natetomsk <47526426+natetomsk@users.noreply.github.com>
2019-04-23 00:16:39 +0700

    Add files via upload

---
+++ Project-Libra-views/bookshelf/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.main_page, name='index'),
+    path('books/', views.Books.as_view(), name='books'),
+	path('authors/', views.Authors.as_view(), name='authors'),
+    #path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'), 
+]
